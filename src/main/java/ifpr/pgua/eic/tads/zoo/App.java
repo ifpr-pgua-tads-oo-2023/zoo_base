3c206b3f8b2c462f3e756ddfe7ba1fd1d804b183
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2023-05-23 11:52:01 -0300

    codigo inicial

---
+++ src/main/java/ifpr/pgua/eic/tads/zoo/App.java
@@ -0,0 +1,13 @@
+package ifpr.pgua.eic.tads.zoo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
